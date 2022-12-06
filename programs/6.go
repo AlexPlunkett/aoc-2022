7be12ceebdafa395624c3583ac3de8a34d01dc99
Alex Plunkett <alexthomasplunkett@gmail.com>
2022-12-06 11:23:58 +0000

    day 6

---
+++ programs/6.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	contents, _ := os.ReadFile("./inputs/6.txt")
+	input := string(contents)
+
+	part1 := scanMarker(input, 4)
+	part2 := scanMarker(input, 14)
+
+	fmt.Printf("\nPart 1: %s", part1)
+	fmt.Printf("\nPart 2: %s", part2)
+}
+
+func scanMarker(buffer string, length int) int {
+	var position int
+
+	for i := 0; i < len(buffer)-(length-1); i++ {
+		isValid := true
+		for _, x := range buffer[i : i+length] {
+			if strings.Count(buffer[i:i+length], string(x)) > 1 {
+				isValid = false
+			}
+		}
+		if isValid {
+			position = i + length
+			break
+		}
+	}
+
+	return position
+}
